23a029c972ab1c81f3aa32f6556e93d0bce7f5f7
hiromireyes <hiromi.reyes@itsvc.edu.mx>
2026-03-02 15:02:28 -0600

    primer commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClienteService } from './services/cliente';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Clientes</h1>
+    <ul>
+      <li *ngFor="let cliente of clientes">
+        {{ cliente.nombre }}
+      </li>
+    </ul>
+  `
 })
-export class App {
-  protected readonly title = signal('FitApp');
+export class App implements OnInit {
+
+  clientes: any[] = [];
+
+  constructor(private clienteService: ClienteService) {}
+
+  ngOnInit() {
+    this.clienteService.getClientes().subscribe(data => {
+      console.log(data);
+      this.clientes = data;
+    });
+  }
 }
