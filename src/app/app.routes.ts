019b2f53dab39d4fa1fabec8003098bb89c816ab
adolfocruz-jpg <adolfo.cruz@itsvc.edu.mx>
2026-03-02 15:34:51 -0600

    actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Registro } from './registro/registro';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'registro', component: Registro},
 ];
---
+++ src/app/registro/registro.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro.html',
+  styleUrl: './registro.css',
+})
+export class Registro {
+
+}
