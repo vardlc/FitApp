d98a32b5cb07e675e398c746419afb682e1ac1aa
vardlc <victoradrianramirezdelacruz@gmail.com>
2026-03-05 16:59:04 -0600

    datos

--- src/app/registro/registro.ts
+++ src/app/registro/registro.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
 export class Registro {
+
+  nombre = "";
+  correo = "";
+  password = "";
+  confirmarPassword = "";
+
+  formCompleto(): boolean {
+    return (
+      this.nombre.trim() !== "" &&
+      this.correo.trim() !== "" &&
+      this.password.trim() !== "" &&
+      this.confirmarPassword.trim() !== ""
+    );
+  }
 
 }
