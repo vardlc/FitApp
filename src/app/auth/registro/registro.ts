8a6b50f40278225177eed09280ed7605414bf608
adolfocruz-jpg <adolfo.cruz@itsvc.edu.mx>
2026-04-27 15:01:14 -0600

    Actu

--- src/app/auth/registro/registro.ts
+++ src/app/auth/registro/registro.ts
@@ -2,30 +2,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ClienteService } from '../../services/cliente';
 import { Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Importante para el *ngIf
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  // Agregamos CommonModule para poder usar *ngIf en el HTML
   imports: [FormsModule, RouterModule, CommonModule], 
   templateUrl: './registro.html',
   styleUrls: ['./registro.css'],
 })
 export class Registro {
-  // --- Propiedades del formulario ---
-  rol = 'cliente'; // <--- ESTA ES LA QUE FALTABA
+  rol = 'cliente';
   nombre = '';
   correo = '';
   password = '';
   confirmPassword = '';
-  
-  // Campos de Salud (Solo para cliente)
   edad = 0;
   estatura = 0;
   peso = 0;
-
-  // Campo para Coach (Ejemplo opcional)
   especialidad = '';
 
   constructor(
@@ -39,7 +33,6 @@ export class Registro {
       return;
     }
 
-    // Construimos el objeto dinámicamente
     const datos: any = {
       nombre: this.nombre,
       correo: this.correo,
@@ -47,13 +40,11 @@ export class Registro {
       rol: this.rol
     };
 
-    // Solo incluimos datos de salud si es cliente
     if (this.rol === 'cliente') {
       datos.edad = this.edad;
       datos.estatura = this.estatura;
       datos.peso = this.peso;
     } else {
-      // Si es coach, podrías enviar campos específicos aquí
       datos.especialidad = this.especialidad;
     }
 
